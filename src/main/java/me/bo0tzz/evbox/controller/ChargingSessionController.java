34d07ee8946335230dd50287a7a103efcb24aee2
Bo0tzz <btdewilligen@gmail.com>
2019-04-04 12:25:49 +0200

    Add controller boilerplate

---
+++ src/main/java/me/bo0tzz/evbox/controller/ChargingSessionController.java
@@ -0,0 +1,42 @@
+package me.bo0tzz.evbox.controller;
+
+import me.bo0tzz.evbox.model.ChargingSession;
+import me.bo0tzz.evbox.model.ChargingSessionSummary;
+import me.bo0tzz.evbox.model.ChargingStation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class ChargingSessionController {
+
+    /**
+     * Submit a new charging session to a station.
+     * @param station the station for which to start a session.
+     * @return the started charging session.
+     */
+    @PostMapping(value = "/chargingSession", produces = "application/json")
+    public ResponseEntity<ChargingSession> submitChargingSession(ChargingStation station) {
+        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
+    }
+
+    /**
+     * Stop a charging session.
+     * @param sessionId the charging session to stop.
+     * @return the stopped charging session.
+     */
+    @PutMapping(value = "/chargingSession/{sessionId}", produces = "application/json")
+    public ResponseEntity<ChargingSession> stopChargingSession(@PathVariable int sessionId) {
+        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
+    }
+
+    /**
+     * Get a summary of recent charging sessions.
+     * @return a summary of charging sessions for the last minute.
+     */
+    @GetMapping(value = "/chargingSessions", produces = "application/json")
+    public ResponseEntity<ChargingSessionSummary> getChargingSessions() {
+        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
+    }
+
+}
