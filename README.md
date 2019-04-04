0772fa299acb5dc7aabccac83f4aac553480b2e4
Bo0tzz <btdewilligen@gmail.com>
2019-04-04 11:31:09 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # EVBox
 My solution to the EVBox backend coding assignment
+
+The boilerplate for this project was generated using start.spring.io. 
+The assignment is in `assignment.pdf`
---
+++ src/main/java/me/bo0tzz/evbox/EvboxApplication.java
@@ -0,0 +1,13 @@
+package me.bo0tzz.evbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvboxApplication.class, args);
+	}
+
+}
