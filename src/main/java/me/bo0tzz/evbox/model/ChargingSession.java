3bd79c1b56e35d11611bce333c2b9d70967eb1c6
Bo0tzz <btdewilligen@gmail.com>
2019-04-04 12:07:07 +0200

    Add model classes

---
+++ src/main/java/me/bo0tzz/evbox/model/ChargingSession.java
@@ -0,0 +1,37 @@
+package me.bo0tzz.evbox.model;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Data
+public class ChargingSession {
+
+    /**
+     * Unique identifier of the charging session
+     */
+    final int id;
+
+    /**
+     * The ID of the charging station where this session happened
+     */
+    final int stationId;
+
+    /**
+     * The timestamp at which the charging session started
+     */
+    final Date startedAt;
+
+    /**
+     * The status of the charging session
+     */
+    @Setter(AccessLevel.NONE)
+    ChargingStatus status = ChargingStatus.IN_PROGRESS;
+
+    public void finishCharging() {
+        this.status = ChargingStatus.FINISHED;
+    }
+
+}
---
+++ src/main/java/me/bo0tzz/evbox/model/ChargingStatus.java
@@ -0,0 +1,8 @@
+package me.bo0tzz.evbox.model;
+
+public enum ChargingStatus {
+
+    IN_PROGRESS,
+    FINISHED;
+
+}
